c89def3aae984d1e8f9e67d10bcab273c0316281
snakeplayer <chauche.benoit@gmail.com>
2015-12-01 10:24:49 +0100

    swagg
    prpre

---
+++ WebMGSRama/pages/inscription.php
@@ -0,0 +1,25 @@
+<?php
+//Insersion du fichier de fonction
+include_once "../functions/dbFunctions.php";
+
+//Définition du nom de la page
+$titlePage = "Inscription";
+
+//Insersion du header HTML
+include_once "../view/HeadPage.php";
+?>
+
+<section id="SectionPage">
+    <header>
+        Inscription
+    </header>
+</section>
+<aside id="AsidePage">
+    <?php
+    include_once "../view/AsidePage.php";
+    ?>
+</aside>
+
+<?php
+include_once "../view/FootPage.php";
+?>
