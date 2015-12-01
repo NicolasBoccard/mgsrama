c5aa037dcfe7fe1185ad44ef2698465217c2e92b
snakeplayer <chauche.benoit@gmail.com>
2015-12-01 11:04:54 +0100

    Création du formulaire d'inscription (HTML)
    Sérieux, elle sert à quoi cette description ?

--- WebMGSRama/pages/inscription.php
+++ WebMGSRama/pages/inscription.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Page        : inscription.php
  * Description : This page is the page where the users can register themselves.
@@ -11,7 +10,7 @@
 //Insersion du fichier de fonction
 include_once "../functions/dbFunctions.php";
 
-//D�finition du nom de la page
+//Définition du nom de la page
 $titlePage = "Inscription";
 
 //Insersion du header HTML
@@ -22,6 +21,62 @@ include_once "../view/HeadPage.php";
     <header>
         Inscription
     </header>
+    <form method="POST" action="#">
+        <table>
+            <tr>
+                <td>
+                    <label for="nom">Nom :</label>
+                </td>
+                <td>
+                    <input type="text" name="nom">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="prenom">Prénom :</label>
+                </td>
+                <td>
+                    <input type="text" name="prenom">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="pseudo">Pseudo :</label>
+                </td>
+                <td>
+                    <input type="text" name="pseudo">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email :</label>
+                </td>
+                <td>
+                    <input type="email" name="email">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="password">Mot de passe :</label>
+                </td>
+                <td>
+                    <input type="password" name="password">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="confirmPassword">Confirmer le mot de passe :</label>
+                </td>
+                <td>
+                    <input type="password" name="confirmPassword">
+                </td>
+            </tr>
+        </table>
+        <div id="ButtonForm">
+            <input type="submit" name="discard" value="Annuler" class="submit"> 
+            <input type="submit" name="submit" value="Confirmer" class="submit">
+        </div>
+    </form>
 </section>
 <aside id="AsidePage">
     <?php
