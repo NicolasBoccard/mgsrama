7aed7591bb857b602dd754602e93d9b4060421e8
NicolasBoccard <nico.boccard.cfpt@gmail.com>
2015-12-01 10:30:44 +0100

    Ajout de l'entête pour la page logout.php
    Entête de base

--- WebMGSRama/pages/logout.php
+++ WebMGSRama/pages/logout.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * Page        : logout.php
+ * Description : This page disconnects the user.
+ * Authors     : Chauche Benoit & Boccard Nicolas
+ * Date        : 01.12.2015
+ * Version     : 1.0
+ */
+
 session_start();
 
 if ($_SESSION["idLogged"] != "") {
