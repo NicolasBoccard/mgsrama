7823125af6932beaa07d31bfe7753a2ef4b4cb1b
NicolasBoccard <nico.boccard.cfpt@gmail.com>
2015-12-01 10:29:01 +0100

    Ajout de l'entête pour la page login.php
    Entête de base

--- WebMGSRama/pages/login.php
+++ WebMGSRama/pages/login.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * Page        : login.php
+ * Description : This page is the home page whene the users are connected.
+ * Authors     : Chauche Benoit & Boccard Nicolas
+ * Date        : 01.12.2015
+ * Version     : 1.0
+ */
+
 //Insersion du fichier de fonction
 include_once "../functions/dbFunctions.php";
 
